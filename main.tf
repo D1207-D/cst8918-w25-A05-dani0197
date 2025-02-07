3e834d64466ca6112f06415a9d0805756d307936
Daniyal <133042543+D1207-D@users.noreply.github.com>
2025-02-07 17:21:30 -0500

    Add public IP address resource

--- main.tf
+++ main.tf
@@ -48,3 +48,10 @@ resource "azurerm_resource_group" "main" {
   location = var.region
 }
 
+resource "azurerm_public_ip" "main" {
+  name                = "${var.labelPrefix}-public-ip"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  allocation_method   = "Dynamic"
+}
+
